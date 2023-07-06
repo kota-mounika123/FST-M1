95d4c8d31a8da570c960f3164d87055f83e50b8a
kota-mounika123 <137294877+kota-mounika123@users.noreply.github.com>
2023-07-06 16:39:14 +0530

    Add files via upload

---
+++ Java/Activities/Hello.java
@@ -0,0 +1,13 @@
+package Examples;
+
+public class Hello {
+    public static void main(String[] args) {
+        String str1="Hello, World!";
+        System.out.println(str1);
+        int num1 =10;
+        int num2 =10;
+
+        num1 += num2;
+        System.out.println(num1);
+    }
+}
